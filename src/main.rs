f3e7cbd0198178520fdb28683939e1525cd1b5c3
Anton Pavliuk <antonpavliuk@Antons-MacBook-Air.local>
2023-10-13 22:21:30 +0300

    Prepare api, tests, and DB connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::configuration::get_configurations;
+use zero2prod::startup::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let configuration = get_configurations().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
