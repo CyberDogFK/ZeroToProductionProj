a373681929d67393e84d5889e66e5ffab1aebcf7
Anton Pavliuk <antonpavliuk@Antons-MacBook-Air.local>
2023-10-17 13:26:43 +0300

    Finish integrating telemetry

--- src/startup.rs
+++ src/startup.rs
@@ -3,15 +3,17 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(connection);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
