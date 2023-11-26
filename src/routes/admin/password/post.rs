be61e3844314e7cc5c3193a38a5acef9d5824297
Anton Pavliuk <streetreadger@gmail.com>
2023-11-26 15:07:18 +0200

    Add saving changed password, add full change password test

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -3,6 +3,7 @@ use crate::utils::{e500, see_other};
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -14,9 +15,13 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
-        return Ok(see_other("/login"));
+    let user_id = match session.get_user_id().map_err(e500)? {
+        Some(user_id) => user_id,
+        None => {
+            return Ok(see_other("/login"));
+        }
     };
     if form.new_password.expose_secret().len() <= 12 {
         FlashMessage::error("Password must be longer than 12 symbols").send();
@@ -29,5 +34,12 @@ pub async fn change_password(
         .send();
         return Ok(see_other("/admin/password"));
     }
-    todo!()
+    crate::authentication::change_password(
+        user_id,
+    form.0.new_password,
+    &pool)
+        .await
+        .map_err(e500)?;
+    FlashMessage::error("Your password has been changed.").send();
+    Ok(see_other("/admin/password"))
 }
